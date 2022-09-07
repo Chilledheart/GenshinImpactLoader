b2e14f4365d2629c06eb3ee483c9719f3f1dbe80
Chilledheart <hukeyue@hotmail.com>
2022-09-07 15:21:00 +0800

    ui: fix chinese support

--- main.cpp
+++ main.cpp
@@ -227,7 +227,10 @@ int WinMain(HINSTANCE hInstance,
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
     //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
     // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesChineseFull());
+    ImFont* font;
+    font = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\simsun.ttc", 16.0f, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+    IM_ASSERT(font != NULL);
+    font = io.Fonts->AddFontDefault();
     IM_ASSERT(font != NULL);
 
     // Our state
